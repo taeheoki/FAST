2166c13c7bac64ccdcf74fb76803f71b0b80a544
taeheoki <dkqjwl2365@naver.com>
2022-11-03 17:00:04 +0900

    [Collections Framework]feat: HashSet

---
+++ src/test/java/Chapter6/Test/HashSetEx1.java
@@ -0,0 +1,17 @@
+package Chapter6.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx1 {
+    public static void main(String[] args) {
+        Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "4", "4", "4"};
+        Set set = new HashSet();
+
+        for (int i = 0; i < objArr.length; i++) {
+            System.out.println(set.add(objArr[i]));
+        }
+
+        System.out.println(set);
+    }
+}
---
+++ src/test/java/Chapter6/Test/HashSetEx3.java
@@ -0,0 +1,31 @@
+package Chapter6.Test;
+
+import java.util.HashSet;
+
+public class HashSetEx3 {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add("abc");
+        set.add("abc");
+        set.add(new Person("David", 10));
+        set.add(new Person("David", 10));
+
+        System.out.println(set);
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ":" + age;
+    }
+}

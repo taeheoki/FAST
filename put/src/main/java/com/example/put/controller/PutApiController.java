21c12eb6fb49e9a6db53a5b0f22862e6ec632248
taeheoki <dkqjwl2365@naver.com>
2022-11-23 22:46:52 +0900

    [JAVA SPRING]feat: PUT METHOD

---
+++ put/src/main/java/com/example/put/controller/PutApiController.java
@@ -0,0 +1,15 @@
+package com.example.put.controller;
+
+import com.example.put.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PutApiController {
+
+    @PutMapping("/put/{userId}")
+    public PostRequestDto put(@RequestBody PostRequestDto postRequestDto, @PathVariable(name = "userId") Long id) {
+        System.out.println(id);
+        return postRequestDto;
+    }
+}
